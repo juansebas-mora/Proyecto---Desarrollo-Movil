482bf2590b4b7e2a817cc3d3b6a358149a47c64e
Juan Sebastian Mora Rodriguez <juansebas.mora@urosario.edu.co>
2026-04-30 14:39:36 -0500

    feat: Dialog con opciones de registro o consulta de solicitud de acceso a visitantes .dev.JuanMora

--- Android_URumbox/app/src/main/java/com/example/urumbox/AccessMainViewModel.kt
+++ Android_URumbox/app/src/main/java/com/example/urumbox/AccessMainViewModel.kt
@@ -1,5 +1,33 @@
 package com.example.urumbox
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class AccessMainViewModel : ViewModel()
+sealed class AccessMainEvent {
+    object ShowAddVisitorDialog : AccessMainEvent()
+    object NavigateToRequest : AccessMainEvent()
+    object NavigateToConsult : AccessMainEvent()
+}
+
+class AccessMainViewModel : ViewModel() {
+
+    private val _uiEvent = MutableLiveData<AccessMainEvent?>()
+    val uiEvent: LiveData<AccessMainEvent?> = _uiEvent
+
+    fun onAddVisitorClicked() {
+        _uiEvent.value = AccessMainEvent.ShowAddVisitorDialog
+    }
+
+    fun onRegisterRequestClicked() {
+        _uiEvent.value = AccessMainEvent.NavigateToRequest
+    }
+
+    fun onConsultRequestsClicked() {
+        _uiEvent.value = AccessMainEvent.NavigateToConsult
+    }
+
+    fun onEventConsumed() {
+        _uiEvent.value = null
+    }
+}
